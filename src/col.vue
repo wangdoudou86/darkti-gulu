b0876f84cd4c3c44acfc16f17a347068db652cb8
darkTi <892184375@qq.com>
2020-04-17 18:43:57 +0800

    完成row和col的雏形

---
+++ src/col.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="col" :class="[span && `col-${span}`]">
+       <slot></slot>
+   </div>
+</template>
+<script>
+export default {
+    name: 'GuluCol',
+    props:{
+        span: {
+            type: [Number, String]  //意味这两种格式都可以
+        }
+    }
+}
+</script>
+<style lang="scss">
+.col{
+    height: 100px;
+    width: 50%;
+    background-color: grey;
+    border: 1px solid green;
+    $class-prefix: col-;   //定义class的前缀
+    @for $n from 1 through 24 {  //$n就是传进来的span
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;  //乘100%就会变成百分数
+      }
+    }
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="row">
+       <slot></slot>
+   </div>
+</template>
+<script>
+export default {
+    name: 'GuluRow'
+}
+</script>
+<style lang="scss">
+.row{
+    display: flex;
+}
+</style>
