178909acbefbfdba28d826c6cfa09c4e32d4ba30
darkTi <892184375@qq.com>
2020-05-13 20:27:44 +0800

    元素基本样式完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="d-popover" @click="onClick" ref="popover">
+    <div class="d-popover" @click="onClick" ref="popover" :class="positionClass">
         <div class="content-wrapper" v-if="visible" ref="contentWrapper">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block;">
             <slot></slot>
         </span>
     </div>
@@ -11,9 +11,24 @@
 <script>
 export default {
     name: 'DarkPopover',
+    props: {
+        position: {
+            type: String,
+            default: 'top',
+            validator(value){
+                return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+            }
+        }
+    },
     data(){
         return{
             visible: false
+        }
+    },
+    computed: {
+        positionClass(){
+            let { position } = this
+            return [position && `position-${position}`]
         }
     },
     methods:{
@@ -26,8 +41,10 @@ export default {
         },
         documentEvent(e){
             //给document的绑定函数一个执行范围，当它发现点击的范围在内容区域时，就不做任何操作
-            if(this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target) ){return}
+            if(this.$refs.contentWrapper && (this.$refs.contentWrapper===e.target || this.$refs.contentWrapper.contains(e.target)) ){return}
             this.close()
+            console.log('出发啦');
+            
         },
        
         open(){
@@ -44,30 +61,55 @@ export default {
         },
         onClick(e){
             //只有点击按钮元素才执行下列操作，加上这句会比较保险
-            if(this.$refs.triggerWrapper.contains(e.target)){
+            // if(this.$refs.triggerWrapper.contains(e.target)){
                 if(this.visible){
                     this.close()
                 }else{
+                    console.log('click button');
+                    
                     this.open()
                 }
-            }
+            // }
             
         }
     }
 }
 </script>
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .d-popover{
     display: inline-block;
     vertical-align: top;
     position: relative;
 }
 .content-wrapper{
-    border: 1px solid green;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
     position: absolute;
     // bottom: 100%;
     // left: 0;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    filter: drop-shadow(0 0 3px rgba(51,51,51,0.4));
+    background-color: white;
     transform: translateY(-100%);
+    padding: .5em 1em;
+    margin-top: -10px;
+    max-width: 20em;
+    word-break: break-all;
+    &::before,&::after{
+        content: '';
+        display: block;
+        border: 10px solid transparent;
+        position: absolute;
+        left: 10px;
+    }
+    &::before{
+        border-top-color: black;
+        top: 100%;
+    }
+    &::after{
+        border-top-color: white;
+        top: calc(100% - 1px);
+    }
 }
 </style>
