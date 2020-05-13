c648c27469d37e0c8928a4a9a1516622a7bac581
darkTi <892184375@qq.com>
2020-05-13 18:15:07 +0800

    功能基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="d-popover" @click.stop="xxx">
+    <div class="d-popover" @click="onClick" ref="popover">
         <div class="content-wrapper" v-if="visible" ref="contentWrapper">
             <slot name="content"></slot>
         </div>
@@ -16,32 +16,42 @@ export default {
             visible: false
         }
     },
-    mounted(){
-        // console.log(this.$refs.triggerWrapper,'triggerWrapper');
-        
-    },
     methods:{
-        xxx(){
-            this.visible = !this.visible
-            if(this.visible){
-                setTimeout(()=>{
-                    const {contentWrapper, triggerWrapper} = this.$refs
-                    document.body.appendChild(contentWrapper)
-                    const {width, height, top, bottom, left, right} = triggerWrapper.getBoundingClientRect()
-                    contentWrapper.style.top = top + window.scrollY + 'px'
-                    contentWrapper.style.left = left + window.scrollX + 'px'
-                    let eventHandler = ()=>{
-                        this.visible = false
-                        console.log('document的关闭')
-                        document.removeEventListener('click', eventHandler)
-                        console.log('删除x函数')
-                    }
-                    document.addEventListener('click', eventHandler)
-                },0)
-                
-            }else{
-                console.log('关闭');
+        positionContent(){
+            const {contentWrapper, triggerWrapper} = this.$refs
+            document.body.appendChild(contentWrapper)
+            const {width, height, top, bottom, left, right} = triggerWrapper.getBoundingClientRect()
+            contentWrapper.style.top = top + window.scrollY + 'px'
+            contentWrapper.style.left = left + window.scrollX + 'px'
+        },
+        documentEvent(e){
+            //给document的绑定函数一个执行范围，当它发现点击的范围在内容区域时，就不做任何操作
+            if(this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target) ){return}
+            this.close()
+        },
+       
+        open(){
+            this.visible = true
+            setTimeout(()=>{
+                this.positionContent()
+                document.addEventListener('click', this.documentEvent)
+            },0)
+        },
+        close(){
+            //只要关闭内容元素，就要把document上的事件移除
+            this.visible = false
+            document.removeEventListener('click', this.documentEvent)
+        },
+        onClick(e){
+            //只有点击按钮元素才执行下列操作，加上这句会比较保险
+            if(this.$refs.triggerWrapper.contains(e.target)){
+                if(this.visible){
+                    this.close()
+                }else{
+                    this.open()
+                }
             }
+            
         }
     }
 }
